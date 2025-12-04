11b9a84a10e67cb6a9a42a982015dc15c84a1f9e
MRX <kydev1323@gmail.com>
2025-12-04 11:28:10 +0800

    Testing Nuxt

---
+++ app/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <!-- Your Navbar Component or direct HTML here -->
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/contact">Contact</NuxtLink>
+      </nav>
+    </header>
+    <main>
+      <NuxtPage />
+    </main>
+    <footer>
+      <!-- Your Footer Component or direct HTML here -->
+    </footer>
+  </div>
+</template>
